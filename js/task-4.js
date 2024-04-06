018e2a7a83d350795f64a2bca52ce928ed097393
KonstantinKhamilov <134136455+KonstantinKhamilov@users.noreply.github.com>
2024-04-06 08:20:52 +0300

    Выполненно здание №4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const registerForm = document.querySelector(".login-form");
+registerForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const mail = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (mail === "" || password === "") {
+    return alert(`All form fields must be filled in`);
+  } else {
+    console.log(`Email: ${mail}, Password: ${password}`);
+  }
+  form.reset();
+}
