f6a21b6b037aa2f3d8de42cc38eda0cad667fabc
KonstantinKhamilov <134136455+KonstantinKhamilov@users.noreply.github.com>
2024-04-07 09:22:56 +0300

    Выполненно задание №5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,25 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+// Выбираем виджет по классу '.widget'
+const widget = document.querySelector(".widget");
+
+// Внутри виджета выбираем элемент, который отображает текущий цвет
+const colorSpan = widget.querySelector(".color");
+
+// Внутри виджета выбираем кнопку по классу '.change-color'
+const button = widget.querySelector(".change-color");
+
+// Добавляем обработчик события "click" на кнопку
+button.addEventListener("click", function (event) {
+  // Отменяем стандартное поведение кнопки
+  event.preventDefault();
+
+  // Генерируем случайный цвет в формате HEX
+  const randomColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+
+  // Применяем сгенерированный цвет к фону виджета
+  widget.style.backgroundColor = randomColor;
+
+  // Отображаем сгенерированный цвет в тексте
+  colorSpan.textContent = randomColor;
+});
